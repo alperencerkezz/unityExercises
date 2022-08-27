dc32536d7a43ee6c97e3c9e1e3303bfd1864182d
alperencerkezz <alperencerkez@marun.edu.tr>
2022-08-27 10:46:11 +0300

    Update MoveLeftX.cs

--- Challenge 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ Challenge 3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -21,7 +21,7 @@ public class MoveLeftX : MonoBehaviour
         if (!playerControllerScript.gameOver)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
-        }
+        } 
 
         // If object goes off screen that is NOT the background, destroy it
         if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
